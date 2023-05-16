efb26755a6740ecd3dc4459f1149c2d5b1e27eaa
Beldun <scrimix_re@icloud.com>
2023-05-17 02:55:54 +0600

    hw1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Remarkable } from "remarkable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {value: "My first hw in 4m"}
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  getRawMarkup() {
+    const md = new Remarkable()
+    return {__html: md.render(this.state.value)};
+  }
+
+  render() {
+    return(
+      <div className="MarkdownEditor">
+        <h3>Входные данные</h3>
+        <label htmlFor="markdown-content">
+          Введите что-то в формате markdown
+        </label>
+        <textarea id="markdown-content"
+          onChange={this.handleChange}
+          defaultValue={this.state.value}
+        />
+        <h3>Вывод</h3>
+        <div className="content"
+          dangerouslySetInnerHTML={this.getRawMarkup()}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default MarkdownEditor;
